ae50a8b3cecb4586d510b612a0ef8a6b1f8a1332
Rae Allen Tura <rntura@student.apc.edu.ph>
2026-02-23 12:30:05 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Suppress false positive warning for public folder assets
+        if (warning.code === 'UNRESOLVED_IMPORT') return
+        warn(warning)
+      }
+    }
+  }
 })
